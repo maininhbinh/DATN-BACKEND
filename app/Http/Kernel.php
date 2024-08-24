76f55d6ab74a7f8728851fe0f1f3ebfbaa7aeadd
samejkon <lequocphaikql@gmail.com>
2024-08-24 11:26:51 +0700

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\TrimStrings::class,
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
@@ -68,6 +68,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'check.status' => \App\Http\Middleware\CheckOrderStatus::class
+        'check.status' => \App\Http\Middleware\CheckOrderStatus::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\api\UserCouponController;
 use App\Http\Controllers\api\VariantController;
 use App\Http\Controllers\api\VariantOptionController;
 use App\Http\Controllers\api\VNPayController;
+use App\Http\Middleware\AdminMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,152 +38,155 @@ use App\Http\Controllers\api\VNPayController;
 //     return $request->user();
 // });
 
+
 Route::prefix('auth')->group(function () {
     Route::post('signup', [AuthController::class, 'signup']);
     Route::post('verifyOTP', [AuthController::class, 'verifyOTP']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
+Route::middleware("admin")->group(function () {
 
-Route::prefix('user')->group(function () {
-    Route::get('list', [UserController::class, 'index']);
-    Route::get('profile', [UserController::class, 'profile'])->middleware('auth:sanctum');
-    Route::post('', [UserController::class, 'store']);
-    Route::get('{id}', [UserController::class, 'edit']);
-    Route::post('/{id}', [UserController::class, 'update']);
-    Route::delete('/{id}', [UserController::class, 'delete']);
-});
+    Route::prefix('user')->group(function () {
+        Route::get('list', [UserController::class, 'index']);
+        Route::get('profile', [UserController::class, 'profile'])->middleware('auth:sanctum');
+        Route::post('', [UserController::class, 'store']);
+        Route::get('{id}', [UserController::class, 'edit']);
+        Route::post('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'delete']);
+    });
 
-Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
-    Route::get('', [CartController::class, 'index']);
-    Route::post('add', [CartController::class, 'store']);
-    Route::put('/{id}', [CartController::class, 'updateQuantity']);
-    Route::delete('/{id}', [CartController::class, 'destroy']);
-});
+    Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
+        Route::get('', [CartController::class, 'index']);
+        Route::post('add', [CartController::class, 'store']);
+        Route::put('/{id}', [CartController::class, 'updateQuantity']);
+        Route::delete('/{id}', [CartController::class, 'destroy']);
+    });
 
-Route::prefix('brand')->group(function () {
-    Route::get('', [BrandController::class, 'index']);
-    Route::post('', [BrandController::class, 'store']);
-    Route::get('{id}', [BrandController::class, 'edit']);
-    Route::post('{id}', [BrandController::class, 'update']);
-    Route::delete('{id}', [BrandController::class, 'destroy']);
-});
+    Route::prefix('brand')->group(function () {
+        Route::get('', [BrandController::class, 'index']);
+        Route::post('', [BrandController::class, 'store']);
+        Route::get('{id}', [BrandController::class, 'edit']);
+        Route::post('{id}', [BrandController::class, 'update']);
+        Route::delete('{id}', [BrandController::class, 'destroy']);
+    });
 
-Route::prefix('category')->group(function () {
-    Route::get('', [CategoryController::class, 'index']);
-    Route::post('', [CategoryController::class, 'store']);
-    Route::get('{id}', [CategoryController::class, 'edit']);
-    Route::get('show/{id}', [CategoryController::class, 'show']);
-    Route::post('{id}', [CategoryController::class, 'update']);
-    Route::delete('{id}', [CategoryController::class, 'destroy']);
-});
+    Route::prefix('category')->group(function () {
+        Route::get('', [CategoryController::class, 'index']);
+        Route::post('', [CategoryController::class, 'store']);
+        Route::get('{id}', [CategoryController::class, 'edit']);
+        Route::get('show/{id}', [CategoryController::class, 'show']);
+        Route::post('{id}', [CategoryController::class, 'update']);
+        Route::delete('{id}', [CategoryController::class, 'destroy']);
+    });
 
-Route::prefix('detail')->group(function () {
-    Route::get('', [DetailController::class, 'index']);
-    Route::post('', [DetailController::class, 'store']);
-    Route::get('/{id}', [DetailController::class, 'edit']);
-    Route::post('/{id}', [DetailController::class, 'update']);
-    Route::delete('/{id}', [DetailController::class, 'delete']);
-    Route::post('/{id}/restore', [DetailController::class, 'restore']);
-});
+    Route::prefix('detail')->group(function () {
+        Route::get('', [DetailController::class, 'index']);
+        Route::post('', [DetailController::class, 'store']);
+        Route::get('/{id}', [DetailController::class, 'edit']);
+        Route::post('/{id}', [DetailController::class, 'update']);
+        Route::delete('/{id}', [DetailController::class, 'delete']);
+        Route::post('/{id}/restore', [DetailController::class, 'restore']);
+    });
 
-Route::prefix('attribute')->group(function () {
-    Route::get('', [AttributeController::class, 'index']);
-    Route::post('', [AttributeController::class, 'store']);
-    Route::get('/{id}', [AttributeController::class, 'edit']);
-    Route::post('/{id}', [AttributeController::class, 'update']);
-    Route::delete('/{id}', [AttributeController::class, 'delete']);
-    Route::post('/{id}/restore', [AttributeController::class, 'restore']);
-});
+    Route::prefix('attribute')->group(function () {
+        Route::get('', [AttributeController::class, 'index']);
+        Route::post('', [AttributeController::class, 'store']);
+        Route::get('/{id}', [AttributeController::class, 'edit']);
+        Route::post('/{id}', [AttributeController::class, 'update']);
+        Route::delete('/{id}', [AttributeController::class, 'delete']);
+        Route::post('/{id}/restore', [AttributeController::class, 'restore']);
+    });
 
-Route::prefix('value')->group(function () {
-    Route::get('', [ValueController::class, 'index']);
-    Route::post('', [ValueController::class, 'store']);
-    Route::get('/{id}', [ValueController::class, 'edit']);
-    Route::post('/{id}', [ValueController::class, 'update']);
-    Route::delete('/{id}', [ValueController::class, 'delete']);
-    Route::post('/{id}/restore', [ValueController::class, 'restore']);
-});
+    Route::prefix('value')->group(function () {
+        Route::get('', [ValueController::class, 'index']);
+        Route::post('', [ValueController::class, 'store']);
+        Route::get('/{id}', [ValueController::class, 'edit']);
+        Route::post('/{id}', [ValueController::class, 'update']);
+        Route::delete('/{id}', [ValueController::class, 'delete']);
+        Route::post('/{id}/restore', [ValueController::class, 'restore']);
+    });
 
-Route::prefix('role')->group(function () {
-    Route::get('', [RoleController::class, 'index']);
-    Route::post('', [RoleController::class, 'store']);
-    Route::get('/{id}', [RoleController::class, 'edit']);
-    Route::post('/{id}', [RoleController::class, 'update']);
-    Route::delete('/{id}', [RoleController::class, 'delete']);
-    Route::post('/{id}/restore', [RoleController::class, 'restore']);
-});
+    Route::prefix('role')->group(function () {
+        Route::get('', [RoleController::class, 'index']);
+        Route::post('', [RoleController::class, 'store']);
+        Route::get('/{id}', [RoleController::class, 'edit']);
+        Route::post('/{id}', [RoleController::class, 'update']);
+        Route::delete('/{id}', [RoleController::class, 'delete']);
+        Route::post('/{id}/restore', [RoleController::class, 'restore']);
+    });
 
-Route::prefix('product')->group(function () {
-    Route::get('', [ProductController::class, 'index']);
-    Route::post('', [ProductController::class, 'store']);
-    Route::get('/{slug}', [ProductController::class, 'show']);
-    Route::get('/home/{feat}', [ProductController::class, 'featProducts']);
-});
+    Route::prefix('product')->group(function () {
+        Route::get('', [ProductController::class, 'index']);
+        Route::post('', [ProductController::class, 'store']);
+        Route::get('/{slug}', [ProductController::class, 'show']);
+        Route::get('/home/{feat}', [ProductController::class, 'featProducts']);
+    });
 
-Route::prefix('slider')->group(function () {
+    Route::prefix('slider')->group(function () {
 
-    Route::post('/', [SlideController::class, 'store']);
-    Route::get('/', [SlideController::class, 'show']);
-    Route::delete('/{id}', [SlideController::class, 'destroy']);
-    Route::get('/{id}', [SlideController::class, 'edit']);
-    Route::post('/{id}', [SlideController::class, 'update']);
-});
+        Route::post('/', [SlideController::class, 'store']);
+        Route::get('/', [SlideController::class, 'show']);
+        Route::delete('/{id}', [SlideController::class, 'destroy']);
+        Route::get('/{id}', [SlideController::class, 'edit']);
+        Route::post('/{id}', [SlideController::class, 'update']);
+    });
 
-Route::prefix('order')->middleware('auth:sanctum')->group(function () {
-    Route::get('user', [OrderController::class, 'getAllOrder']);
-    Route::get('detail/{id}', [OrderController::class, 'getOrderDetail']);
-    Route::get('', [OrderController::class, 'index']);
-    Route::post('', [OrderController::class, 'placeOrder']);
-    Route::get('{id}', [OrderController::class, 'show']);
-    Route::put('update/status/{id}', [OrderController::class, 'updateStatus'])->middleware('check.status');
-    Route::post('process-payment/{order_id}', [OrderController::class, 'processPayment']); //đã lấy vào id và qua 1 hàm mới để chọn method thanh toán
-});
+    Route::prefix('order')->middleware('auth:sanctum')->group(function () {
+        Route::get('user', [OrderController::class, 'getAllOrder']);
+        Route::get('detail/{id}', [OrderController::class, 'getOrderDetail']);
+        Route::get('', [OrderController::class, 'index']);
+        Route::post('', [OrderController::class, 'placeOrder']);
+        Route::get('{id}', [OrderController::class, 'show']);
+        Route::put('update/status/{id}', [OrderController::class, 'updateStatus'])->middleware('check.status');
+        Route::post('process-payment/{order_id}', [OrderController::class, 'processPayment']); //đã lấy vào id và qua 1 hàm mới để chọn method thanh toán
+    });
 
-Route::prefix('variant')->group(function () {
-    Route::get('', [VariantController::class, 'index']);
-    Route::post('', [VariantController::class, 'store']);
-    Route::get('/{id}', [VariantController::class, 'edit']);
-    Route::post('/{id}', [VariantController::class, 'update']);
-    Route::delete('/{id}', [VariantController::class, 'destroy']);
-    Route::post('/{id}/restore', [VariantController::class, 'restore']);
-});
+    Route::prefix('variant')->group(function () {
+        Route::get('', [VariantController::class, 'index']);
+        Route::post('', [VariantController::class, 'store']);
+        Route::get('/{id}', [VariantController::class, 'edit']);
+        Route::post('/{id}', [VariantController::class, 'update']);
+        Route::delete('/{id}', [VariantController::class, 'destroy']);
+        Route::post('/{id}/restore', [VariantController::class, 'restore']);
+    });
 
-Route::post('option', [VariantController::class, 'show']);
+    Route::post('option', [VariantController::class, 'show']);
 
-Route::prefix('variant_option')->group(function () {
-    Route::get('', [VariantOptionController::class, 'index']);
-    Route::post('', [VariantOptionController::class, 'store']);
-    Route::get('/{id}', [VariantOptionController::class, 'edit']);
-    Route::post('/{id}', [VariantOptionController::class, 'update']);
-    Route::delete('/{id}', [VariantOptionController::class, 'destroy']);
-    Route::post('/{id}/restore', [VariantOptionController::class, 'restore']);
-});
+    Route::prefix('variant_option')->group(function () {
+        Route::get('', [VariantOptionController::class, 'index']);
+        Route::post('', [VariantOptionController::class, 'store']);
+        Route::get('/{id}', [VariantOptionController::class, 'edit']);
+        Route::post('/{id}', [VariantOptionController::class, 'update']);
+        Route::delete('/{id}', [VariantOptionController::class, 'destroy']);
+        Route::post('/{id}/restore', [VariantOptionController::class, 'restore']);
+    });
 
-Route::prefix('payment')->group(function () {
-    Route::post('momo/{orderId}', [PaymentController::class, 'momo_payment']);
-    Route::get('callback', [PaymentController::class, 'fallBack']);
-    Route::post('stripe/{order_id}', [StripeController::class, 'stripePayment']);
-    Route::post('vnpay/{order_id}', [VNPayController::class, 'vnpay_payment']);
-    Route::get('/vnpay-return', [VNPayController::class, 'returnCallBack']);
-});
-Route::prefix('coupon')->group(function () {
-    Route::post('apply', [CouponController::class, 'apply']);
-    Route::get('', [CouponController::class, 'index']);
-    Route::post('', [CouponController::class, 'store']);
-    Route::get('/{id}', [CouponController::class, 'edit']);
-    Route::post('/{id}', [CouponController::class, 'update']);
-    Route::delete('/{id}', [CouponController::class, 'destroy']);
-    Route::post('apply', [CouponController::class, 'apply']);
-});
+    Route::prefix('payment')->group(function () {
+        Route::post('momo/{orderId}', [PaymentController::class, 'momo_payment']);
+        Route::get('callback', [PaymentController::class, 'fallBack']);
+        Route::post('stripe/{order_id}', [StripeController::class, 'stripePayment']);
+        Route::post('vnpay/{order_id}', [VNPayController::class, 'vnpay_payment']);
+        Route::get('/vnpay-return', [VNPayController::class, 'returnCallBack']);
+    });
+    Route::prefix('coupon')->group(function () {
+        Route::post('apply', [CouponController::class, 'apply']);
+        Route::get('', [CouponController::class, 'index']);
+        Route::post('', [CouponController::class, 'store']);
+        Route::get('/{id}', [CouponController::class, 'edit']);
+        Route::post('/{id}', [CouponController::class, 'update']);
+        Route::delete('/{id}', [CouponController::class, 'destroy']);
+        Route::post('apply', [CouponController::class, 'apply']);
+    });
 
-Route::prefix('filter')->group(function () {
-    Route::post('', [ProductController::class, 'filter']);
-    Route::get('/search', [ProductController::class, 'search']);
-});
+    Route::prefix('filter')->group(function () {
+        Route::post('', [ProductController::class, 'filter']);
+        Route::get('/search', [ProductController::class, 'search']);
+    });
 
-Route::middleware('auth:sanctum')->prefix('comment')->group(function () {
-    Route::get('/products/{productId}/comments', [CommentController::class, 'index']);
-    Route::post('', [CommentController::class, 'store']);
-    Route::delete('/{id}', [CommentController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->prefix('comment')->group(function () {
+        Route::get('/products/{productId}/comments', [CommentController::class, 'index']);
+        Route::post('', [CommentController::class, 'store']);
+        Route::delete('/{id}', [CommentController::class, 'destroy']);
+    });
 });
